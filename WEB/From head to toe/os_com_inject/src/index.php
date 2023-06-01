fbbeba499ff13ca422a051550c07a49912ad886d
aminnazri00 <72528347+aminnazri00@users.noreply.github.com>
2023-06-01 21:45:05 +0800

    Add files via upload

---
+++ WEB/From head to toe/os_com_inject/src/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sanity Check in Space Part I: Luke Vader Strikes Back</title>
+  </head>
+  <body>
+    <h1>Welcome to page one of Sanity Check in space!</h1>
+    <img src="res/robots.txt.jpg" alt="image">
+  </body>
+</html>
---
+++ WEB/From head to toe/os_com_inject/src/space.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Sanity Check in Space part V - Liam, Last Son of Krypton</title>
+  </head>
+  <body>
+    <h1>Welcome to the final page of Sanity Check in Space. This is the ultimate challenge. This is: Ping but in Space.</h1>
+    <img src="res/kryp.jpg" alt="truth" width="500" height="400">
+
+    <form method="post">
+      <label for="website">This tool pings websites, but in space.</label>
+      <input type="text" id="website" name="website" required>
+      <br>
+      <button type="submit" name="ping">Space Ping</button>
+    </form>
+    <?php
+    if(isset($_POST["ping"])) {
+        // $site = $_POST["website"];
+        // $cmd = "curl " . $site;
+        // $out = shell_exec($cmd);
+        // $smarty->assign('cmdout', nl2br($out));
+
+
+        $url = $_POST["website"];
+        $output = shell_exec('ping -c 1 '. $url);
+        echo "<pre>$output</pre>";
+    }
+    ?>
+
+  </body>
+</html>
